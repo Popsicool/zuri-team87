583f49f3add0509aea96f854219f2524f958bef8
Popsicool <akinolasamson1234@gmail.com>
2022-07-25 12:50:10 +0100

    add user authentication

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name= 'home'
 urlpatterns = [
     path('', views.HomeView, name= 'all'),
+    path('register', views.register, name= 'register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
